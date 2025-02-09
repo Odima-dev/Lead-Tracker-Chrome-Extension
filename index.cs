4e6e619f7dc889c38dcec6181ac0716cf1a4228f
Odima-dev <vincentodima22@gmail.com>
2025-02-09 20:30:30 +0300

    styling page

--- index.cs
+++ index.cs
@@ -1,5 +1,15 @@
+* {
+    box-sizing: border-box;
+}
+
 body {
     margin: 0;
     padding: 10px;
     font-family: Arial, Helvetica, sans-serif;
+}
+
+input {
+    width: 100%;
+    padding-left: 10px;
+    padding-right: 10px;
 }
