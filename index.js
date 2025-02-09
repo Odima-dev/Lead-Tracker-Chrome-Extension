c31878c46374309696e54326f7d91bd5f46bff25
Odima-dev <vincentodima22@gmail.com>
2025-02-09 23:01:22 +0300

    adding event listener

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function saveInput() {
+    console.log("Button is clicked")
+}
+
+let inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function() {
+    
+})
+
+
+
