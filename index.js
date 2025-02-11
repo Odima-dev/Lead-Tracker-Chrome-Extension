a1840d2e1f940037e01f4f1bd8d6410d8ad7f7b5
Odima-dev <vincentodima22@gmail.com>
2025-02-12 01:49:31 +0300

    using arrays

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let myLeads = [];
-let inputEl = document.getElementById("input-el");
-
-let inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked")
+    myLeads.push("www.awesomelead.com" );
+    console.log(myLeads)
 })
 
 
