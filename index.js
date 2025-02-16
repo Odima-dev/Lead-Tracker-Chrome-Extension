21b7e515231c28977406333adf49e4cf0ad1817a
Odima-dev <vincentodima22@gmail.com>
2025-02-16 20:48:11 +0300

    using localStorage function

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const ulEl = document.getElementById("ul-el")
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-console.log(leadsFromLocalStorage); 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
 
 
 inputBtn.addEventListener("click", function() {
