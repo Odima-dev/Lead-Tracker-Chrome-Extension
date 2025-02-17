09dcc7f2e578fc2d918e99e70e5f5e22fc12f4a7
Odima-dev <vincentodima22@gmail.com>
2025-02-17 23:22:27 +0300

    improving app by using function parameters and arguments

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ if (leadsFromLocalStorage) {
     render(myLeads);
 }
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -28,12 +28,12 @@ function renderLeads() {
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
+    render(myLeads)
 })
