e5a78822808a4b5ec51686ad841c965f9690a916
Odima-dev <vincentodima22@gmail.com>
2025-02-18 00:18:24 +0300

    using Chrome API to get tabs

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ if (leadsFromLocalStorage) {
 }
 
 
+
 tabBtn.addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
